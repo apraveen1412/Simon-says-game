67bd1c78da3ef6b4586f96d43b85d9505af4f6d3
apraveen1412 <aaraveetipraveen@gmail.com>
2025-07-23 20:26:25 +0530

    Basic UI without functionalities

---
+++ app.js
@@ -0,0 +1,71 @@
+let h1=document.createElement('h1');
+let h2=document.createElement('h2');
+let container=document.createElement('div');
+let subContainer1=document.createElement('div');
+let subContainer2=document.createElement('div');
+let red=document.createElement('div');
+let green=document.createElement('div');
+let orange=document.createElement('div');
+let violet=document.createElement('div');
+let body=document.querySelector('body');
+
+red.setAttribute('class', 'red' );
+green.setAttribute('class', 'green' );
+orange.setAttribute('class', 'orange' );
+violet.setAttribute('class', 'violet' );
+red.setAttribute('type', 'button');
+green.setAttribute('type', 'button');
+orange.setAttribute('type', 'button' );
+violet.setAttribute('type', 'button' );
+container.setAttribute('id', 'container');
+subContainer1.setAttribute('class', 'subContainer');
+subContainer2.setAttribute('class', 'subContainer');
+subContainer1.setAttribute('id', 'subContainer1');
+subContainer2.setAttribute('id', 'subContainer2');
+h1.innerText='Simon Game'
+h2.innerText='Press any key to start the game';
+
+subContainer1.append(red);
+subContainer1.append(green);
+subContainer2.append(orange);
+subContainer2.append(violet);
+container.append(subContainer1);
+container.append(subContainer2);
+body.append(h1);
+body.append(h2);
+body.append(container);
+
+
+
+let level=0;
+let gameStarted=false;
+let gameSequence=[];
+let userSequence=[];
+let colors=['red', 'green', 'orange', 'violet'];
+
+document.addEventListener('keypress', function() {
+    if(gameStarted==false){
+        gameStarted=true;
+        levelUp();
+    }
+});
+
+function btnFlash(btn){
+    btn.classList.add('flash');
+    setTimeout(function(){
+        btn.classList.remove('flash');
+    }, 1000);
+}
+
+function levelUp(){
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=colors[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+
+    btnFlash(randBtn);
+}
